0a916e62e111b73d96ef3555580e5bdf0b981517
LiuJQ9674Git <455298138@qq.com>
2016-10-19 20:42:19 +0800

    asyncdomo debug

--- redux-examples/async/webpack.config.js
+++ redux-examples/async/webpack.config.js
@@ -2,28 +2,57 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './index'
-  ],
-  output: {
+  //devtool: 'cheap-module-eval-source-map',
+  /**
+   entry: [
+   'webpack-hot-middleware/client',
+
+   './src/index'
+   ],
+   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+
+  },**/
+  entry: {
+    app:path.join(__dirname, 'src'),
+
+    vendors: ['react','react-dom','redux','react-redux','redux-thunk','redux-logger',
+      'isomorphic-fetch','babel-polyfill']
+  },
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: '[name].js',
+    publicPath: '/dist/'
+
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    //new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    //这个使用uglifyJs压缩你的js代码
+    //new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+    /**,
+     new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      sourceMap: true,//这里的soucemap 不能少，可以在线上生成soucemap文件，便于调试
+      mangle: true
+    })**/
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      exclude: /node_modules/,
-      include: __dirname
-    }]
+    loaders: [
+      {
+        test:/\.js?$/,
+        exclude:/node_modules/,
+        loader:'babel',
+        query:{
+          presets:['react','es2015']
+        }
+      }
+    ]
   }
 }
 
@@ -39,7 +68,7 @@ if (fs.existsSync(reduxSrc) && fs.existsSync(reduxNodeModules)) {
   // Compile Redux from source
   module.exports.module.loaders.push({
     test: /\.js$/,
-    loaders: ['babel'],
+    loaders: [ 'babel' ],
     include: reduxSrc
   })
 }
